1dd713f7e9e356f10deba3f728984e67e1d18630
LinSongjian <19864553795@163.com>
2026-05-31 16:39:45 +0800

    feat: cp mosaic

---
+++ pkg/mosaic/abe/arith_pbc.go
@@ -0,0 +1,235 @@
+// +build pbc
+
+package abe
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"math/big"
+	"strconv"
+
+	"bc_abe/pkg/mosaic/abe/pbc"
+)
+
+func NewPoint() Point {
+	return new(PbcPoint)
+}
+
+func NewCurve() Curve {
+	return new(PbcCurve)
+}
+
+// point on pbc library
+type PbcPoint struct {
+	p     *pbc.Element
+	P     string `json:"p"`
+	Group string `json:"group"`
+}
+
+func (p *PbcPoint) isPoint() {}
+
+func (p *PbcPoint) GetP() string {
+	return p.P
+}
+
+func (p *PbcPoint) GetGroup() string {
+	return p.Group
+}
+
+// PbcPoint type to its json representation
+func (p *PbcPoint) ToJsonObj() Point {
+	p.P = Encode(string(p.p.Bytes()))
+	return p
+}
+
+// PbcPoint type from its json representation
+func (p *PbcPoint) OfJsonObj(curve Curve) Point {
+	tmp := curve.NewPointOn(p.Group).(*PbcPoint)
+	p.p = tmp.p.SetBytes([]byte(Decode(p.P)))
+	return p
+}
+
+// curve on pbc library
+type PbcCurve struct {
+	pairing   *pbc.Pairing
+	params    *pbc.Params
+	maxrndexp *big.Int
+	Params    string `json:"params"`
+	Maxrndexp string `json:"maxrndexp"`
+	Seed 			string `json:"seed"`
+}
+
+func (curve *PbcCurve) isCurve() {}
+
+// PbcCurve type to its json representation
+func (curve *PbcCurve) ToJsonObj() Curve {
+	curve.Params = Encode(curve.params.String())
+	curve.Maxrndexp = Encode(curve.maxrndexp.Bytes())
+	return curve
+}
+
+// PbcCurve type from its json representation
+func (curve *PbcCurve) OfJsonObj() Curve {
+	curve.params = pbc.NewParamsFromString(Decode(curve.Params))
+	curve.pairing = pbc.NewPairing(Decode(curve.params))
+	curve.maxrndexp = new(big.Int).SetBytes(Decode(curve.Maxrndexp))
+	return curve
+}
+
+func (curve *PbcCurve) SetSeed(seed string) Curve {
+	curve.Seed = seed
+	return curve
+}
+
+func (curve *PbcCurve) InitRng() Curve {
+	return curve
+}
+
+// new point instance on a curve
+func (curve *PbcCurve) NewPointOn(group string) Point {
+	return &PbcPoint{
+		p: curve.pairing.NewUncheckedElement(group),
+	}
+}
+
+// new random exponent
+func (curve *PbcCurve) NewRandomExp() *big.Int {
+	rng := rand.Reader
+	x, _ := rand.Int(rng, curve.maxrndexp)
+	return x
+}
+
+// new random point on a group ("G1", "G2", "GT") in the bilinear pairing
+func (curve *PbcCurve) NewRandomPointOn(seed string, group string) Point {
+	tmp := curve.NewPointOn(group).(*PbcPoint)
+	return &PbcPoint{
+		p:     tmp.p.Rand(),
+		Group: group,
+	}
+}
+
+// unit on group ("G1", "G2", "GT") in the bilinear pairing
+func (curve *PbcCurve) UnitOnGroup(group string) Point {
+	tmp := curve.NewPointOn(group).(*PbcPoint)
+	return &PbcPoint{
+		p:     tmp.p.Set1(),
+		Group: group,
+	}
+}
+
+// hash to group ("G1", "G2", "GT") in the bilinear pairing
+func (curve *PbcCurve) HashToGroup(x string, group string) Point {
+	hx := sha256.Sum256([]byte(x))
+	tmp := curve.NewPointOn(group).(*PbcPoint)
+	return &PbcPoint{
+		p:     tmp.p.SetFromHash(hx[:]),
+		Group: group,
+	}
+}
+
+// hash to group ("G1", "G2", "GT") in the bilinear pairing
+func (curve *PbcCurve) HashToPow(x string, g Point) Point {
+	hx := sha256.Sum256([]byte(x))
+	return curve.Pow(g, new(big.Int).SetBytes(hx[:]))
+}
+
+// group operation
+func (curve *PbcCurve) Inv(g1 Point) Point {
+	tmp := curve.NewPointOn(g1.GetGroup()).(*PbcPoint)
+	return &PbcPoint{
+		p:     tmp.p.Invert(g1.(*PbcPoint).p),
+		Group: g1.GetGroup(),
+	}
+}
+
+// group operation
+func (curve *PbcCurve) Mul(g1 Point, g2 Point) Point {
+	tmp := curve.NewPointOn(g1.GetGroup()).(*PbcPoint)
+	return &PbcPoint{
+		p:     tmp.p.Mul(g1.(*PbcPoint).p, g2.(*PbcPoint).p),
+		Group: g1.GetGroup(),
+	}
+}
+
+// group operation
+func (curve *PbcCurve) Div(g1 Point, g2 Point) Point {
+	tmp := curve.NewPointOn(g1.GetGroup()).(*PbcPoint)
+	return &PbcPoint{
+		p:     tmp.p.Div(g1.(*PbcPoint).p, g2.(*PbcPoint).p),
+		Group: g1.GetGroup(),
+	}
+}
+
+// group operation
+func (curve *PbcCurve) Pow(g1 Point, e1 *big.Int) Point {
+	tmp := curve.NewPointOn(g1.GetGroup()).(*PbcPoint)
+	tmp.p = tmp.p.PowBig(g1.(*PbcPoint).p, new(big.Int).Abs(e1))
+	if e1.Sign() < 0 {
+		tmp.p.ThenInvert()
+	}
+	return &PbcPoint{
+		p:     tmp.p,
+		Group: g1.GetGroup(),
+	}
+}
+
+// pairing
+func (curve *PbcCurve) Pair(g1 Point, g2 Point) Point {
+	group := "GT"
+	tmp := curve.NewPointOn(group).(*PbcPoint)
+	return &PbcPoint{
+		p:     tmp.p.Pair(g1.(*PbcPoint).p, g2.(*PbcPoint).p),
+		Group: group,
+	}
+}
+
+// group operation
+func (curve *PbcCurve) Pow2(g1 Point, e1 *big.Int, g2 Point, e2 *big.Int) Point {
+	return curve.Mul(curve.Pow(g1, e1), curve.Pow(g2, e2))
+}
+
+// group operation
+func (curve *PbcCurve) Pow3(g1 Point, e1 *big.Int, g2 Point, e2 *big.Int, g3 Point, e3 *big.Int) Point {
+	return curve.Mul(curve.Pow(g1, e1), curve.Mul(curve.Pow(g2, e2), curve.Pow(g3, e3)))
+}
+
+// pairing
+func (curve *PbcCurve) ProdPair(g1 []Point, g2 []Point) Point {
+	tmp := curve.UnitOnGroup("GT")
+	for i := 0; i < len(g1); i++ {
+		tmp = curve.Mul(tmp, curve.Pair(g1[i], g2[i]))
+	}
+	return tmp
+}
+
+// new random secret vector of length n
+// if zerosecret is true, the first component of the vector is zero
+func (curve *PbcCurve) NewRandomSecret(n int, zerosecret bool) []*big.Int {
+	s := make([]*big.Int, n)
+	for i := 0; i < n; i++ {
+		s[i] = curve.NewRandomExp()
+	}
+	if zerosecret {
+		s[0] = new(big.Int).SetInt64(0)
+	}
+	return s
+}
+
+func GetPbcCurve(curveStr string) string {
+	curve := new(PbcCurve)
+
+	if curveStr == "SS512" {
+		bits := 160
+		curve.params = pbc.GenerateA(uint32(bits), 512)
+		curve.maxrndexp = new(big.Int).SetBit(new(big.Int).SetInt64(0), int(bits-1), 1)
+	}
+
+	if curveStr[0:2] == "BN" {
+		bits, _ := strconv.ParseInt(string(curveStr[2:]), 10, 32)
+		curve.params = pbc.GenerateF(uint32(bits))
+		curve.maxrndexp = new(big.Int).SetBit(new(big.Int).SetInt64(0), int(bits-1), 1)
+	}
+
+	curve.Encode()
+	return JsonObjToStr(curve)
+}
---
+++ pkg/mosaic/abe/crypto.go
@@ -0,0 +1,214 @@
+package abe
+
+import (
+	"crypto/sha256"
+	"math/big"
+	"strings"
+
+	"bc_abe/pkg/mosaic/abe/log"
+)
+
+// new random organization
+func NewRandomOrg(curve Curve) (org *Org) {
+	org = &Org{
+		Crv: curve,
+		G1:  curve.NewRandomPointOn("G1"),
+		G2:  curve.NewRandomPointOn("G2"),
+	}
+	org.E = curve.Pair(org.G1, org.G2)
+	return
+}
+
+// Json API for NewRandomOrg
+func NewRandomOrgJson(curveJson string) string {
+	curve := NewCurveOfJsonStr(curveJson).OfJsonObj()
+	org := NewRandomOrg(curve)
+	return Encode(JsonObjToStr(org.ToJsonObj()))
+}
+
+// new random authority
+func NewRandomAuth(org *Org) (authkeys *AuthKeys) {
+	authprv := &AuthPrv{
+		Org:   org,
+		Alpha: org.Crv.NewRandomExp(),
+		Y:     org.Crv.NewRandomExp()}
+
+	authpub := &AuthPub{
+		Org:    org,
+		Ealpha: org.Crv.Pow(org.E, authprv.Alpha),
+		G1y:    org.Crv.Pow(org.G1, authprv.Y)}
+
+	authkeys = &AuthKeys{
+		AuthPub: authpub, AuthPrv: authprv}
+	return
+}
+
+// Json API for NewRandomAuth
+func NewRandomAuthJson(orgJson string) string {
+	org := NewOrgOfJsonStr(orgJson).OfJsonObj()
+	authkeys := NewRandomAuth(org)
+	return Encode(JsonObjToStr(authkeys.ToJsonObj()))
+}
+
+// new random userkey
+func NewRandomUserkey(user string, attr string, authprv *AuthPrv) (userattrs *UserAttrs) {
+	org := authprv.Org
+
+	userattrs = new(UserAttrs)
+	userattrs.User = user
+	userattrs.Userkey = make(map[string]*Userkey)
+	userattrs.Coeff = make(map[string][]int)
+
+	nbit := 32
+	attrs := getBagOfBitsAttrs(attr, nbit)
+	for _, attr := range attrs {
+		huser := org.Crv.HashToGroup(user, "G2")
+		hattr := org.Crv.HashToGroup(attr, "G2")
+		t := org.Crv.NewRandomExp()
+
+		userkey := &Userkey{
+			Org: org,
+			K:   org.Crv.Pow3(org.G2, authprv.Alpha, huser, authprv.Y, hattr, t),
+			KP:  org.Crv.Pow(org.G1, t),
+		}
+		userattrs.Userkey[attr] = userkey
+		userattrs.Coeff[attr] = []int{}
+	}
+	return
+}
+
+// Json API for NewRandomUserkey
+func NewRandomUserkeyJson(user string, attr string, authprvJson string) string {
+	authprv := NewAuthPrvOfJsonStr(authprvJson).OfJsonObj()
+	userattrs := NewRandomUserkey(user, attr, authprv)
+	return Encode(JsonObjToStr(userattrs.ToJsonObj()))
+}
+
+// new random secret
+func NewRandomSecret(org *Org) (secret Point) {
+	secret = org.Crv.NewRandomPointOn("GT")
+	//secret = org.Crv.HashToGroup(org.Crv.seed, "GT")
+	return secret
+}
+
+// Json API for NewRandomSecret
+func NewRandomSecretJson(orgJson string) string {
+	org := NewOrgOfJsonStr(orgJson).OfJsonObj()
+	p := NewRandomSecret(org)
+	return Encode(JsonObjToStr(p.ToJsonObj()))
+}
+
+func SecretHash(secret Point) string {
+	secret_hash := sha256.Sum256([]byte(secret.ToJsonObj().GetP()))
+	return Encode(string(secret_hash[:]))
+}
+
+func GetOrgFromAuthPubs(authpubs *AuthPubs) *Org {
+	// all authorities in the policy from the same organization
+	authpub := new(AuthPub)
+	for _, authpubtmp := range authpubs.AuthPub {
+		authpub = authpubtmp
+		break
+	}
+	return authpub.Org
+}
+
+// encrypt a secret according to a given policy
+// the function requires the public keys of the authorities mentioned in the policy
+func Encrypt(secret Point, policy string, authpubs *AuthPubs) (ct *Ciphertext) {
+	org := GetOrgFromAuthPubs(authpubs)
+	curve := org.Crv
+
+	ap := buildAccessPolicy(policy)
+	s := curve.NewRandomSecret(len(ap.Vars), false)
+	w := curve.NewRandomSecret(len(ap.Vars), true)
+	sh_s := computeShares(s, ap.M)
+	sh_w := computeShares(w, ap.M)
+	log.Debug("s0 secret %s", s[0])
+	for attr, rows := range ap.Row {
+		log.Debug("shares of secret s0 for %s", attr)
+		for _, i := range rows {
+			log.Debug("share on row %d: %s", i, sh_s[i])
+		}
+	}
+
+	S0 := curve.Pow(org.E, s[0])
+	C0 := curve.Mul(secret, S0)
+
+	C := make(map[string][][]Point)
+	for attr, rows := range ap.Row {
+		auth := strings.SplitN(attr, "@", 2)[1]
+		authpub := authpubs.AuthPub[auth]
+		hattr := curve.HashToGroup(attr, "G2")
+
+		C[attr] = make([][]Point, len(rows))
+		for k, i := range rows {
+			tx := curve.NewRandomExp()
+
+			C[attr][k] = make([]Point, 4)
+			C[attr][k][0] = curve.Pow2(org.E, sh_s[i], authpub.Ealpha, tx)
+			C[attr][k][1] = curve.Inv(curve.Pow(org.G1, tx))
+			C[attr][k][2] = curve.Pow2(org.G1, sh_w[i], authpub.G1y, tx)
+			C[attr][k][3] = curve.Pow(hattr, tx)
+		}
+	}
+
+	ct = &Ciphertext{
+		Org: org, Policy: policy, C0: C0, C: C}
+	return
+}
+
+// Json API for Encrypt
+func EncryptJson(secretJson string, policy string, authpubsJson string) string {
+	secret := NewPointOfJsonStr(secretJson)
+	authpubs := NewAuthPubsOfJsonStr(authpubsJson).OfJsonObj()
+	org := GetOrgFromAuthPubs(authpubs)
+	secret.OfJsonObj(org.Crv)
+	ct := Encrypt(secret, policy, authpubs)
+	return Encode(JsonObjToStr(ct.ToJsonObj()))
+}
+
+// decrypt a ciphertext
+// the function requires the list of user attributes to use
+// and the corresponding collection of userkey
+func Decrypt(ct *Ciphertext, userattrs *UserAttrs) (secret Point) {
+	org := ct.Org
+	curve := org.Crv
+	huser := curve.HashToGroup(userattrs.User, "G2")
+
+	S0 := curve.UnitOnGroup("GT")
+	for attr, cs := range userattrs.Coeff {
+		for k, c := range cs {
+			if c != 0 {
+				userkey := userattrs.Userkey[attr]
+				tmp := curve.ProdPair(
+					[]Point{ct.C[attr][k][1], ct.C[attr][k][2], userkey.KP},
+					[]Point{userkey.K, huser, ct.C[attr][k][3]})
+				tmp = curve.Mul(tmp, ct.C[attr][k][0])
+				tmp = curve.Pow(tmp, big.NewInt(int64(c)))
+				S0 = curve.Mul(S0, tmp)
+			}
+		}
+	}
+
+	secret = curve.Div(ct.C0, S0)
+	return
+}
+
+// Json API for Decrypt
+func DecryptJson(ctJson string, userattrsJson string) string {
+	ct := NewCiphertextOfJsonStr(ctJson).OfJsonObj()
+	userattrs := NewUserAttrsOfJsonStr(userattrsJson).OfJsonObj()
+	secret := Decrypt(ct, userattrs)
+	return secret.ToJsonObj().GetP()
+}
+
+func PolicyOfCiphertext(ct *Ciphertext) string {
+	return ct.Policy
+}
+
+// Json API extracting policy from ciphertexts
+func PolicyOfCiphertextJson(ctJson string) string {
+	ct := NewCiphertextOfJsonStr(ctJson).OfJsonObj()
+	return ct.Policy
+}
